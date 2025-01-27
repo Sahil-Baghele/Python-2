52bd1fda9e9edb3f52679249c7727f5b5fc779f6
Sahil-Baghele <163012140+Sahil-Baghele@users.noreply.github.com>
2025-01-27 17:49:35 +0530

    commit

---
+++ Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "7 years": "https://www.youtube.com/watch?v=Q0bnAmfGQC8&pp=ygUHNyB5ZWFycw%3D%3D",
+    "cradles": "https://www.youtube.com/watch?v=KBtk5FUeJbk&pp=ygUHY3JhZGxlcw%3D%3D",
+    "Anime": "https://www.youtube.com/watch?v=5NzfqW_Yt6Y&pp=ygUUYmxpbmcgYmFuZyBiYW5nIGJvcm4%3D",
+    "Anime 2": "https://www.youtube.com/watch?v=1tk1pqwrOys&pp=ygUMa2Fpa2FpIGtpdGFu",
+        }
